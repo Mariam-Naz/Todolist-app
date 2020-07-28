67ccffa325bbb640642df1eec69ecbdeee8cf503
unknown <mariamnaz212@gmail.com>
2020-07-29 02:59:34 +0500

    Delete item from the array

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,16 +1,44 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
+import ToDoList from "./ToDoList";
+import Button from "./Button";
+import Heading from "./Heading";
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
-function ToDoList(props){
-  const [isDone , setIsDone] = useState(false);
-
-  function handleDelete(){
-    setIsDone(prevValue=>{
-      return !prevValue;
-    });
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
   }
-  return <div onClick={handleDelete}> 
-  <li style={{textDecoration: isDone ? "line-through" : "none"}}>{props.todoItem}</li>
-</div>
-}
+  function addItem() {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
 
-export default ToDoList;
+  function deleted(id){
+    setItems(prevValue =>{
+        return prevValue.filter((item , index)=>{
+          return index !== id;
+        })
+    })
+  }
+  return (
+    <div className="container">
+     <Heading />
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <Button addItem={addItem} />
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem,index) => (
+            <ToDoList key={index} id={index} todoItem = {todoItem} onChecked={deleted} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default App;
