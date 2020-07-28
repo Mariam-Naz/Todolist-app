4a0592563c64cc42234811d2300b553fa525737e
unknown <mariamnaz212@gmail.com>
2020-07-28 23:16:16 +0500

    delete line through

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
 import React from "react";
 
-function Heading(){
-  return  <div className="heading">
-  <h1>To-Do List</h1>
-  </div>
-=======
-import React from 'react';
-
-function Heading(){
-  return <div className="heading">
-  <h1>To-Do List</h1>
-</div>
->>>>>>> 271fe2cfee711a31256937fbaa87727b5c3d7279
+function Heading() {
+  return (
+    <div className="heading">
+      <h1>To-Do List</h1>
+    </div>
+  );
 }
 
 export default Heading;
